ea00fb50a3b09fd39d9b7a9f95970862dd4fc51c
jagoqui <jagoqui.gomez@gmail.com>
2024-08-27 18:14:54 -0500

    fix: linter issues.

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -4,7 +4,7 @@ const OfflinePage: React.FC = () => {
   return (
     <div>
       <h1>You are offline</h1>
-      <p>It seems you don't have an internet connection.</p>
+      <p>It seems you don&apos;t have an internet connection.</p>
     </div>
   );
 };
